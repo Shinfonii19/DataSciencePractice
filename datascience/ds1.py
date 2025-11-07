b8e84f2ee9c60bec63fe9179e3a361e359825763
Ron Jerald Capiral <jeraldcapiral123@gmail.com>
2025-11-07 22:13:30 +0800

    String Concatenation

--- datascience/ds1.py
+++ datascience/ds1.py
@@ -1,1 +1,21 @@
-print("hello")
+# 1. Creating Strings
+name = "Ron"
+greetings = 'Hello, World'
+
+print(name)
+print(greetings)
+print("---------------")
+# 2. Multi-line Strings
+
+sentence = """Welcome back
+to my
+Youtube Channel"""
+
+print(sentence)
+print("---------------")
+
+# 3. Concatatenation (String + Variable String)
+lname = "Capiral"
+
+print("Hi my name is " + name + " Thankyou!")
+print(greetings + " My name is " + lname)
