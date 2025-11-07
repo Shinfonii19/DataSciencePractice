bad90c81b1716913d3966778e2896da053e670d8
Ron Jerald Capiral <jeraldcapiral123@gmail.com>
2025-11-07 22:22:43 +0800

    String Indexing and Slicing

--- datascience/ds1.py
+++ datascience/ds1.py
@@ -19,3 +19,19 @@ lname = "Capiral"
 
 print("Hi my name is " + name + " Thankyou!")
 print(greetings + " My name is " + lname)
+print("---------------")
+
+# 4. String Indexing and Slicing
+
+word = "Python"
+
+print(word[0])  # 1st letter
+print(word[1])  # 2nd letter
+print(word[2])
+print(word[3])
+print(word[-2])  # last 2nd letter since its negative
+print(word[-1])  # last 1st letter
+
+print(word[0:4])  # 0 is for starting and 2 is for before it
+print(word[-4:-1])
+# summary, the 1st number is the current placement, and also 2nd 2nd number if its negative, but if its positive, before the placement of it
