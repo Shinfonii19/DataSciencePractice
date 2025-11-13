22406c0643f22b16ed4f946346d58dc58105985b
Ron Jerald Capiral <jeraldcapiral123@gmail.com>
2025-11-13 22:26:14 +0800

    Challenge 1: About Me

---
+++ ds1_quiz.py
@@ -0,0 +1,29 @@
+"""
+Challenge 1: About Me
+Instructions:
+1. Create variables for your name, age, and favorite hobby.
+2. Use an f-string to print something like:
+3. Add 1 to your age in the print statement (to show your age next year).
+"""
+name = "Ron"
+age = 22
+hobby = 'Playing Games'
+
+print(f"Hi! I'm {name}, I am {age} years old, and I love {hobby}")
+"""
+Challenge 2: Simple Calculator
+Instructions:
+1. Ask the user for two numbers (using input()).
+2. Print their sum, difference, product, and average
+"""
+
+
+"""
+Challenge 3: String Magic
+Instructions:
+1. Create a string variable with the text "Python is fun!"
+2. Print:
+- The text in uppercase
+- The first 6 letters only
+- The text with "fun" replaced by "awesome"
+"""
